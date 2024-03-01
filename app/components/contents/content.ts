95e7ea986de3e1437beabf8ee3553a9fc4cc157e
bhavish <bhavishp@ivoyant.com>
2024-03-01 23:50:40 +0530

    services section fintech

---
+++ app/components/contents/content.ts
@@ -0,0 +1,22 @@
+export const servicesList = `{
+  "servicesList": [
+    {
+      "id": 1,
+      "name": "Service 1",
+      "description": "Description for Service 1",
+      "bullets": ["Point 1", "Point 2", "Point 3"]
+    },
+    {
+      "id": 2,
+      "name": "Service 2",
+      "description": "Description for Service 2",
+      "bullets": ["Point A", "Point B", "Point C"]
+    },
+    {
+      "id": 3,
+      "name": "Service 3",
+      "description": "Description for Service 3",
+      "bullets": ["Bullet X", "Bullet Y", "Bullet Z"]
+    }
+  ]
+}`;
