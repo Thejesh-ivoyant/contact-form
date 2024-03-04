2e9b85459d754722140a2f2719f2109f00cc383e
Thejesh-ivoyant <127406875+Thejesh-ivoyant@users.noreply.github.com>
2024-03-04 22:59:30 +0530

    msg

--- app/utils/SMS.ts
+++ app/utils/SMS.ts
@@ -26,8 +26,8 @@ export const SMS = async (c_code: string, phone_no:string) => {
 
       const to = `${c_code}${phone_no}`;
       const from = '+14049946986';
-      const smsBody = `Thank you for reaching out to ivoyant, We've received your form submission and are currently reviewing it. 
-      For any urgent inquiries, feel free to reach out to us directly at [info@ivoyant.com] [Ph: +1 (770) 274 4336].
-      https://contact-us-silk-ten.vercel.app/fintech`;
+      const smsBody = `It was great meeting with you at FinTech meet. We're excited to connect and partner with you.You can reach me  directly at sonya@ivoyant.com or  +1 (678) 674 3937.
+      LinkedIn - https://www.linkedin.com/in/sonya-b-235372120
+      Website - https://www.ivoyant.com`;
       await sendSMS(to, from, smsBody);
   };
