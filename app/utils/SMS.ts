161794b2ab559bad514703235ce3733a17bb9a6d
Thejesh-ivoyant <127406875+Thejesh-ivoyant@users.noreply.github.com>
2024-05-28 11:05:04 +0530

    Send grid re structured

--- app/utils/SMS.ts
+++ app/utils/SMS.ts
@@ -26,7 +26,7 @@ export const SMS = async (c_code: string, phone_no:string, name:string) => {
 
       const to = `${c_code}${phone_no}`;
       const from = '+14049946986';
-      const smsBody = `${name}, it was great meeting with you at FinTech meet. We're excited to connect and partner with you. You can reach me directly at:\n\nSonya Beredimas\nsonya@ivoyant.com \n+1 (678) 674 3937\nhttps://www.linkedin.com/in/sonya-b-235372120\nhttps://www.ivoyant.com`;
+      const smsBody = `${name}, it was great meeting with you at ProcureCon meet. We're excited to connect and partner with you. You can reach me directly at:\n\nSonya Beredimas\nsonya@ivoyant.com \n+1 (678) 674 3937\nhttps://www.linkedin.com/in/sonya-b-235372120\nhttps://www.ivoyant.com`;
 
       await sendSMS(to, from, smsBody);
   };
